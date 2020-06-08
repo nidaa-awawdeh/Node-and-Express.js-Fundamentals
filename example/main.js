6b1ebf2bf103f8b75540ae59d562a078d9e583d9
nidaa-awawdeh <nidaaawawdeh@outlook.sa>
2020-06-08 13:59:50 +0300

    adding more example in folder example like module #2

--- example/main.js
+++ example/main.js
@@ -1,2 +1,12 @@
 const helpers = require("./helpers");
+const moreHelpers = require("./moreHelpers");
+const evenMoreHelpers = require("./evenMoreHelpers");
+const { first, second, third } = require("./models");
+
+console.log(moreHelpers.firstName);
 helpers.sayHi();
+moreHelpers.sayHello();
+moreHelpers.sayGoodbye();
+
+
+evenMoreHelpers(); ///  call function 
