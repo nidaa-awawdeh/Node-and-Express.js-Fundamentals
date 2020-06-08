898852c132b8155898487e21876e0e9121d4c7b1
nidaa-awawdeh <nidaaawawdeh@outlook.sa>
2020-06-08 21:49:11 +0300

    add express example folder #8

---
+++ exampleexpress/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+app.get("/", function (request, response) {
+  return response.send("Hello World!");
+});
+
+app.get("/user/:name", function (request, response) {
+  return response.send(`Hello ${request.params.name}`);
+});
+
+app.listen(3000, function () {
+  console.log("Server is listening on port 3000");
+});
